ea3dfe9bf1942dad383d7d6fd630178829cb642e
cesareduardo824 <cesareduardo824@gmail.com>
2018-02-13 23:56:53 -0500

    Parte 2 al 90 % de terminado

--- src/main/java/edu/eci/pdsw/claseNueva.java
+++ src/main/java/edu/eci/pdsw/claseNueva.java
@@ -15,6 +15,8 @@ import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -34,55 +36,54 @@ public class claseNueva extends HttpServlet{
     
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        base=DataSourceStub.getInstance();
+        base=DataSourceStub.getInstance();           
+        Writer responseWriter=resp.getWriter();
+        System.out.println("llego");
+        Optional <Integer> optName = Optional.of(Integer.parseInt(req.getParameter("Iden")));                    
+        Integer name = optName.get();
         try {
-            
-            Writer responseWriter=resp.getWriter();
-            Optional <Integer> optName = Optional.of(Integer.parseInt(req.getParameter("Iden")));
-            Integer name = optName.get();
+
             clien=base.getClientById(name);
+            System.out.println("mirar el usuario"+clien.getName());
+            if(clien.equals(null)){
+                responseWriter.write("Usuario no encontrado"+clien.getSallary());
+                System.out.println("321321312ver"+clien.equals(req));            
+            
+            }else{
+
+
+            System.out.println("llego3");
             String HTML="<!DOCTYPE html>"+
-                  "<html>"+
-                "<head><title>Ejemplo de tabla sencilla</title></head>"+
-                "<body>"+
-                 "<h1>Listado de cursos</h1>"+
-                 "<table>"+
-                "<tr>"+
-                  "<td><strong>Curso</strong></td>"+
-                  "<td><strong>Horas</strong></td>"+
-                  "<td><strong>Horario</strong></td>"+
-                "</tr>"+
-                "<tr>"+
-                  "<td>SS</td>"+
-                  "<td>20</td>"+
-                  "<td>16:00 - 20:00</td>"+
-                "</tr>"+
-                "<tr>"+
-                  "<td>HTML</td>"+
-                  "<td>20</td>"+
-                  "<td>16:00 - 20:00</td>"+
-                "</tr>"+
-                "<tr>"+
-                  "<td>Dreamweaver</td>"+
-                  "<td>60</td>"+
-                  "<td>16:00 - 20:00</td>"+
-                "</tr>"+
-                "</table>"+
-                "</body>"+
-                "</html>";
                     
-                   
+                    "<html>"+
+                    "<head><title>Ejemplo de tabla sencilla</title></head>"+
+                    "<body>"+
+                    "<h1>Listado de cursos</h1>"+
+                    "<table>"+
+                    "<tr>"+
+                    "<td><strong>Nombre</strong></td>"+
+                    "<td><strong>Correo Electronico</strong></td>"+
+                    "<td><strong>Dirrecion</strong></td>"+
+                    "<td><strong>Salario</strong></td>"+
+                    "</tr>"+
+                    "<tr>"+
+                    "<td>"+clien.getName()+"</td>"+
+                    "<td>"+clien.getEmail()+"</td>"+
+                    "<td>"+clien.getAddress()+"</td>"+
+                    "<td>"+clien.getSallary()+"</td>"+
+                    "</tr>"+
+                    "</table>"+
+                    "</body>"+
+                    "</html>";
+                    responseWriter.write(HTML);
+                    responseWriter.flush();                
 
-
-            //responseWriter.write("OK"+name+"!");
-            //responseWriter.write("Correo"+clien.getAddress()+"!");
-            responseWriter.write(HTML);
-            responseWriter.flush();
-            
-            
+            }
         } catch (ClientNotFoundException ex) {
+            resp.setStatus(2);            
             Logger.getLogger(claseNueva.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
         //responseWriter.write("OK"+name1+"!");
         //responseWriter.flush();
     }
